969fdcb8568d2ae998b2ed7d7a38051cbeec9c01
Juls1111 <99805587+Juls1111@users.noreply.github.com>
2023-01-11 19:10:57 +0300

    error corrected

--- assignment_problem.py
+++ assignment_problem.py
@@ -11,7 +11,6 @@ def main():
     # Non-consecutive shifts are not allowed.
     # The optimal assignment maximizes the number of fulfilled shift requests for those with a higher rank.
 
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     num_employees = 5
     num_shifts = 3
     num_days = 7
@@ -19,8 +18,7 @@ def main():
     all_shifts = range(num_shifts)
     all_days = range(num_days)
     employees_per_shift = [1, 2, 1]  # employees_per_shift[i] indicates number of employees required for shift i
-    num_shifts = sum(employees_per_shift)
-  
+ 
   
     priorities = [100, 100, 10, 9, 10]  # priorities[i] indicates rank of employee i
     # shift_requests[i][j][k] indicates whether employee i wants to work shift k on day j
